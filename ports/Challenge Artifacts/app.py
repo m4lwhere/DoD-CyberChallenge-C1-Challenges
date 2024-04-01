6ce3c424f06c7b299385ffbaf834683d04dde678
m4lwhere <m4lwhere@protonmail.com>
2024-04-01 09:03:38 -0400

    latest updates and pushes

--- ports/Challenge Artifacts/app.py
+++ ports/Challenge Artifacts/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'C1{ch3ck_4ll_p0rts!}'
+    return '\nYou found me! Your flag is:\n\nC1{ch3ck_4ll_p0rts!}\n\n'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=51147)  # Using 51147 as an example high-numbered port
